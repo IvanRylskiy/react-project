58a1de3b2d6adad8bb63165ae75e786e50148db3
Ivan <ivan.rylskiy@gmail.com>
2019-12-17 13:37:02 +0700

    date and loader fixes

--- src/components/daily-costs/daily-costs.jsx
+++ src/components/daily-costs/daily-costs.jsx
@@ -4,12 +4,44 @@ import Cost from '../cost';
 import './daily-costs.scss';
 
 class DailyCosts extends React.Component {
+  date = () => {
+    const { costs } = this.props;
+    const months = [
+      'января',
+      'февраля',
+      'марта',
+      'апреля',
+      'мая',
+      'июня',
+      'июля',
+      'августа',
+      'сентября',
+      'октября',
+      'ноября',
+      'декабря'
+    ];
+    const date = new Date(costs[0].date);
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const todayDate = new Date();
+    const todayYear = todayDate.getFullYear();
+    let fullDate = '';
+    if (year === todayYear) {
+      fullDate = day + ' ' + month;
+    } else {
+      fullDate = day + ' ' + month + ', ' + year;
+    }
+    return fullDate;
+  };
+
   render() {
     const { costs, deleteItem, handleInputEdit, editItem } = this.props;
+    const date = this.date();
 
     return (
       <div className="daily-costs">
-        <div className="date">{costs.length ? costs[0].date : ''}</div>
+        <div className="date">{date}</div>
         <div className="costs">
           {costs.map(({ id, category, comment, value }) => (
             <Cost
